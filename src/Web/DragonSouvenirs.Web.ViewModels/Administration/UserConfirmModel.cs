7a6394081de66e9a04b492d0bdf7d6b611197f6c
Stoyan Draganov <stoyandraganov875@gmail.com>
2021-04-26 15:58:07 +0300

    Move messages to GlobalConstants

---
+++ src/Web/DragonSouvenirs.Web.ViewModels/Administration/UserConfirmModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DragonSouvenirs.Web.ViewModels.Administration
+{
+    using DragonSouvenirs.Data.Models;
+    using DragonSouvenirs.Services.Mapping;
+
+    public class UserConfirmModel : IMapFrom<ApplicationUser>
+    {
+        public string Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
