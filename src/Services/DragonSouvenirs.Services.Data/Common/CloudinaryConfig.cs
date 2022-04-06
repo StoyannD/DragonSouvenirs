637416b0ad144525f1bee5074206241e5d4a4868
Stoyan Draganov <stoyandraganov875@gmail.com>
2022-04-06 19:52:40 +0300

    Delete CloudinaryConfig.cs

--- src/Services/DragonSouvenirs.Services.Data/Common/CloudinaryConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DragonSouvenirs.Services.Data.Common
-{
-    public class CloudinaryConfig
-    {
-        public const string CloudName = "dvqk1ngdx";
-        public const string ApiKey = "441343926138621";
-        public const string ApiSecret = "SOJPTiBA9o81IR3THq723pYJ_3c";
-    }
-}
