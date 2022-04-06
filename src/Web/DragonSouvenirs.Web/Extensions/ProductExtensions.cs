7bca38cd844359a1078e2211d622d8dc4b2b12e7
Stoyan Draganov <stoyandraganov875@gmail.com>
2022-04-07 00:35:09 +0300

    Add search string filter extension method

---
+++ src/Web/DragonSouvenirs.Web/Extensions/ProductExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace DragonSouvenirs.Web.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using DragonSouvenirs.Web.ViewModels.Categories;
+
+    public static class ProductExtensions
+    {
+        public static IEnumerable<ProductInCategoryViewModel> FilterBySearchString(
+            this IEnumerable<ProductInCategoryViewModel> products, string searchString)
+        {
+            if (searchString != null)
+            {
+                var searchStringArr = searchString
+                    .Split(new[] { ",", ".", " ", "\\", "/", "|", "!", "?" }, StringSplitOptions.RemoveEmptyEntries);
+
+                return products.Where(p =>
+                    searchStringArr.All(ss => p.Title.ToLower().Contains(ss.ToLower())));
+            }
+
+            return products;
+        }
+    }
+}
