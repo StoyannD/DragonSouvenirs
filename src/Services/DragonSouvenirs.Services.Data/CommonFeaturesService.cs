807534f03a01543942d3f6981af52f9e06755854
Stoyan Draganov <stoyandraganov875@gmail.com>
2022-04-06 19:50:42 +0300

    Add Extension method for enum display attribute

---
+++ src/Services/DragonSouvenirs.Services.Data/CommonFeaturesService.cs
@@ -0,0 +1,52 @@
+﻿namespace DragonSouvenirs.Services.Data
+{
+    using Microsoft.AspNetCore.Http;
+
+    public class CommonFeaturesService : ICommonFeaturesService
+    {
+        public bool IsImageFileTypeValid(IFormFile image)
+        {
+            if (image != null)
+            {
+                var fileType = image.ContentType.Split('/')[1];
+                if (!TypeIsValid(fileType))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public string RenderAddress(
+            string city,
+            string neighborhood,
+            string street,
+            int streetNumber,
+            string apartmentBuilding,
+            string entrance,
+            int floor,
+            int apartmentNumber)
+        {
+            var address = "гр. " + city
+                                 + ", кв. " + neighborhood
+                                 + ", ул. " + street
+                                 + " " + streetNumber;
+
+            address += apartmentBuilding != null
+                ? ", бл. " + apartmentBuilding + " " : string.Empty;
+            address += entrance != null
+                ? ", вх. " + entrance + " " : string.Empty;
+
+            address += ", ет. " + floor
+                                + ", ап. " + apartmentNumber;
+
+            return address;
+        }
+
+        private static bool TypeIsValid(string fileType)
+        {
+            return fileType is "jpeg" or "jpg" or "png";
+        }
+    }
+}
---
+++ src/Services/DragonSouvenirs.Services.Data/ICommonFeaturesService.cs
@@ -0,0 +1,19 @@
+﻿namespace DragonSouvenirs.Services.Data
+{
+    using Microsoft.AspNetCore.Http;
+
+    public interface ICommonFeaturesService
+    {
+        bool IsImageFileTypeValid(IFormFile image);
+
+        string RenderAddress(
+            string city,
+            string neighborhood,
+            string street,
+            int streetNumber,
+            string apartmentBuilding,
+            string entrance,
+            int floor,
+            int apartmentNumber);
+    }
+}
